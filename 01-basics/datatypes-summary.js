e184ebe29cfcf54380dbb90d7e991b19a2394d5a
Shri2710 <64968326+Shri2710@users.noreply.github.com>
2024-01-30 02:50:10 +0000

    summary of dataypes

---
+++ 01-basics/datatypes-summary.js
@@ -0,0 +1,17 @@
+// Primitive 
+
+// 7 types : String, Boolean, Number, Undefined, Null, Symbol, BigInt
+// Used as call by value
+
+//Non Primitive
+
+// Array, Object, Functions
+
+
+const id = Symbol('123');
+const anotherid = Symbol('123');
+
+console.log(id === anotherid);
+
+
+// Primitive (Stack), Non Primitive (Heap)
