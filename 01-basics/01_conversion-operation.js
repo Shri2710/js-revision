bb99561ad782ac026941f4b7c18b4eaf376bc850
Shri2710 <64968326+Shri2710@users.noreply.github.com>
2024-01-30 02:21:50 +0000

    conversion of data-types

---
+++ 01-basics/01_conversion-operation.js
@@ -0,0 +1,16 @@
+let score = 33;
+
+let valueInScore = Number(score);
+
+console.log(typeof score);
+console.log(valueInScore);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+// false => 0
+// null => 0
+// undefined => NaN
+// "" => NaN
+
