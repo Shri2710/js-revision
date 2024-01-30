796f1dff8d20cd6dc3967a3a3082e00057b7cf55
Shri2710 <64968326+Shri2710@users.noreply.github.com>
2024-01-30 02:12:09 +0000

    datatypes

---
+++ 01-basics/01_data-types.js
@@ -0,0 +1,17 @@
+let name = 'Shriganesh';
+let age = 32;
+let isLoggedIn = true;
+
+// number => 2^53
+// bigInt
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+//Object
+
+
+console.log(typeof name); //string
+console.log(typeof null); // object
