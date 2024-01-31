16b4850dee906008358bb94a17cc18205751aa8e
Shri2710 <64968326+Shri2710@users.noreply.github.com>
2024-01-31 01:49:02 +0000

    math and number in js

---
+++ 01-basics/02_number_math.js
@@ -0,0 +1,18 @@
+const score = 100;
+
+const scoreNumber = new Number(100);
+
+// console.log(score);
+// console.log(scoreNumber);
+
+
+// +++++++++++++ Math +++++++++++++++++
+
+console.log(Math.abs(-10));
+console.log(Math.random());
+console.log(Math.floor(Math.random() * 10) + 1 );
+
+const min = 8;
+const max = 15;
+
+console.log(Math.floor(Math.random() * (max - min) + 1) +min);
