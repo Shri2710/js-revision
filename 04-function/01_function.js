7bb16e5343899617dfde7e8c0ae7519e66a5a3c3
Shri2710 <64968326+Shri2710@users.noreply.github.com>
2024-02-01 02:22:26 +0000

    functions

---
+++ 04-function/01_function.js
@@ -0,0 +1,10 @@
+function addTwoNumbers(a,b){
+    console.log(a+b);
+}
+
+function loggedInUser(username="Sam"){
+    return `${username} just logged in`;
+}
+// const result = addTwoNumbers(8,9);
+
+console.log(loggedInUser());
