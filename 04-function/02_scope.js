f4223d741f6e836c121870d53b826642b5a9751b
Shri2710 <64968326+Shri2710@users.noreply.github.com>
2024-02-01 02:34:04 +0000

    scoping in js

---
+++ 04-function/02_scope.js
@@ -0,0 +1,9 @@
+if(true){
+    let a=10;
+    const b=20;
+    var c= 30;
+}
+
+// console.log(a); throws an error because of block scope
+// console.log(b); throws an error because of block scope
+console.log(c);
