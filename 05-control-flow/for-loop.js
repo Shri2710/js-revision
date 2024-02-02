f445911cdb4cc029933862f4b85ff32fc7cec2d6
Shri2710 <64968326+Shri2710@users.noreply.github.com>
2024-02-02 02:35:27 +0000

    for loop with break

---
+++ 05-control-flow/for-loop.js
@@ -0,0 +1,15 @@
+for (let index = 0; index < 10; index++) {
+    const element = index;
+    // console.log(element);
+}
+
+// break and continue
+
+for(let i=0; i<=20;i++){
+    if(i ===5){
+        console.log('Detected 5');
+        break;
+    }
+
+    console.log('i value is',i);
+}
